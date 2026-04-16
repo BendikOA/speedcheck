2928482da10583731c2603562fb8c6017bb5d0fd
Bendik <bendik.andersen@uib.no>
2026-04-16 14:08:54 +0200

    keep meta description under 160 characters

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,7 +42,7 @@
     name: "Turnadus",
     url: "https://turnadus.com",
     description:
-      "Free Pokémon speed tier and turn order tool for VGC and Pokémon Champions. All in one ingame companions and meta analysis. Check turn order, build teams, damage calc, meta overview, VGC team inspiration",
+      "Free Pokémon speed tier and turn order tool for VGC and Pokémon Champions. All in one ingame companion, teambuilder, meta analysis, speed tiers.",
     applicationCategory: "GameApplication",
     operatingSystem: "Web Browser",
     inLanguage: "en",
